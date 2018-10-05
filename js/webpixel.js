99ddcd1ad96d41cf5ed690ccb7e4706187abbb8e
ggeugene <g.eugene.g@outlook.com>
2018-10-05 21:22:02 +0300

    Added webpixel.js file to html HEAD. Moved slider script to webpixel.js

---
+++ js/webpixel.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('#lightSlider').lightSlider({
+        item: 2.5,
+        vertical: true,
+        loop: false,
+        move: 1,
+        verticalHeight: 550,
+        pager: false,
+        controls: false
+    });
+});
